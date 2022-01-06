a7e2151d88d598ba71d9bbfaf44c7a46991b1e99
artie-owlet <artie-owlet@protonmail.com>
2022-01-06 06:30:57 +0300

    fix winsToPermut() algorithm

--- src/wins-to-permut.ts
+++ src/wins-to-permut.ts
@@ -7,10 +7,10 @@ function swap(permutDice: number[], permutSides: number[], dieWins: number[], lD
         const rmId = findDieSide(permutDice, permutSides, gDieId, sideId);
         const beforeLessId = dieWins[sideId] < dieWins.length ?
             findDieSide(permutDice, permutSides, lDieId, w) : permutDice.length;
+        const afterLessId = dieWins[sideId] > 0 ? findDieSide(permutDice, permutSides, lDieId, w - 1) : -1;
+        const afterSameId = sideId > 0 ? findDieSide(permutDice, permutSides, gDieId, sideId - 1) : -1;
 
         if (rmId > beforeLessId) {
-            const afterLessId = dieWins[sideId] > 0 ? findDieSide(permutDice, permutSides, lDieId, w - 1) : -1;
-            const afterSameId = sideId > 0 ? findDieSide(permutDice, permutSides, gDieId, sideId - 1) : -1;
             const insId = Math.max(afterLessId, afterSameId) + 1;
 
             const tmpDieId = permutDice[rmId];
@@ -18,6 +18,17 @@ function swap(permutDice: number[], permutSides: number[], dieWins: number[], lD
             for (let id = rmId; id > insId; --id) {
                 permutDice[id] = permutDice[id - 1];
                 permutSides[id] = permutSides[id - 1];
+            }
+            permutDice[insId] = tmpDieId;
+            permutSides[insId] = tmpSideId;
+        } else if (rmId < afterLessId) {
+            const insId = Math.max(afterLessId, afterSameId);
+
+            const tmpDieId = permutDice[rmId];
+            const tmpSideId = permutSides[rmId];
+            for (let id = rmId; id < insId; ++id) {
+                permutDice[id] = permutDice[id + 1];
+                permutSides[id] = permutSides[id + 1];
             }
             permutDice[insId] = tmpDieId;
             permutSides[insId] = tmpSideId;
